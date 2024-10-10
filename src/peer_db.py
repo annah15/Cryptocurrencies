7ec9cf77bcfc67f47cc3d623645d84e9da152e29
Anna <anna@die-hoeppis.de>
2024-10-10 10:32:42 +0200

    Add functions to reed form and write to peer db file

--- src/peer_db.py
+++ src/peer_db.py
@@ -6,9 +6,15 @@ PEER_DB_FILE = "peers.csv"
 
 def store_peer(peer: Peer, existing_peers: Iterable[Peer] = None):
     # append to file
-    pass
+    with open(PEER_DB_FILE, 'a') as f:
+        f.write(f"{peer.host},{peer.port}\n")
 
 
 def load_peers() -> Set[Peer]:
     # read from file
-    pass
+    with open(PEER_DB_FILE, 'r') as f:
+        peers = set()
+        for line in f:
+            host, port = line.strip().split(',')
+            peers.add(Peer(host, int(port)))
+    return peers
