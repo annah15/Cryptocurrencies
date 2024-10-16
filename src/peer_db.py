8037132472fc0424e5f6557d6b4b0e2ae41d2c9f
Anna <anna@die-hoeppis.de>
2024-10-16 13:50:22 +0200

    Store discovered peers in db

--- src/peer_db.py
+++ src/peer_db.py
@@ -6,9 +6,12 @@ PEER_DB_FILE = "peers.csv"
 
 def store_peer(peer: Peer, existing_peers: Iterable[Peer] = None):
     # append to file
-    with open(PEER_DB_FILE, 'a') as f:
-        f.write(f"{peer.host},{peer.port}\n")
-
+    if existing_peers is None:
+        existing_peers = load_peers()
+    
+    if not peer in existing_peers:
+        with open(PEER_DB_FILE, 'a') as f:
+            f.write(f"{peer.host},{peer.port}\n")
 
 def load_peers() -> Set[Peer]:
     # read from file
